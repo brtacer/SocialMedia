3221ee805dede841f1cf8286eb9a91e207e40e44
brtacer <beratacer125@gmail.com>
2023-03-18 12:51:35 +0300

    add config server

---
+++ api-gateway-service/src/main/java/com/berat/controller/FallbackController.java
@@ -0,0 +1,20 @@
+package com.berat.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+    @GetMapping("/authservice")
+    public ResponseEntity<String> authServiceFallback(){
+        return ResponseEntity.ok("Auth service şu anda hizmet vermemektedir.");
+    }
+    @GetMapping("/userservice")
+    public ResponseEntity<String> userServiceFallback(){
+        return ResponseEntity.ok("User service şu anda hizmet vermemektedir.");
+    }
+}
---
+++ config-server/src/main/java/com/berat/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.berat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class);
+    }
+}
